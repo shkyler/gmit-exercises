4a90d1afed2b83e18b7d48247ff0a18400e8c9b4
Patrick Moore <moorepa@outlook.com>
2018-03-18 21:03:06 +0000

    updated with a comment

--- mean.py
+++ mean.py
@@ -1,7 +1,7 @@
 # Patrick Moore 2018-02-27
 # Iris Data Set - (http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data)
 # Reference for the problem - (https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files) fromt the Python Tutorial
-
+# This is not a problem from the problem sets - this was me trying to get the average of a column from the iris data file
 with open("data/iris.csv", "r") as myfile:                         
   sums = 0
   mean = 0 
