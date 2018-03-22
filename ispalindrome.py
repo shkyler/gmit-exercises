2cd121bb8c1e7f4a0a2f18d9dd706c40762f912e
Patrick Moore <moorepa@outlook.com>
2018-03-22 20:24:10 +0000

    tidying up the repository

--- ispalindrome.py
+++
@@ -1,7 +0,0 @@
-def ispalindrome(string):
-  if string[::-1] == string[:]:
-    print(string + " is a palindrome")
-  else:
-    print(string + " is not a palindrome")
-
-ispalindrome("navan")
--- sumultiply.py
+++
@@ -1,9 +0,0 @@
-def sumultiply(a,b):
-  counter = 0
-  solution = 0
-  while counter != b:
-    solution = solution + a
-    counter = counter + 1
-  return(solution)  
-
-print(sumultiply(100,36))
